cbb86679a5ecefa130260b654b3e351806f17f4b
bmvantunes <bmvantunes@outlook.com>
2017-04-01 14:30:30 +0100

    test(fixtures): remove module.id from our tests
    closes #69

--- test/fixtures/basic-html-basic-scss-and-css/output-expected/basic.component.ts
+++ test/fixtures/basic-html-basic-scss-and-css/output-expected/basic.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
   selector: 'selector',
   template: `Super Simple HTML<div></div><div></div>`,
   styles: [`.my-class{background-color:red;position:-webkit-sticky;position:sticky}.normal-css{background-color:stuff}`]
