adea0470306aad284c81af8a816139a2e026fed9
Bruno Antunes <bmvantunes@users.noreply.github.com>
2017-04-13 12:04:00 +0100

    fix(tasks): Copy .npmignore, .gitignore and LICENSE to lib-dist folder (#91)
    closes #90

--- src/core/tasks/main.task.ts
+++ src/core/tasks/main.task.ts
@@ -3,7 +3,7 @@ import { DeleteDestFolderTask } from './delete-dest-folder.task';
 import { InlineHtmlCssTask } from './inline-html-css.task';
 import { NgcCompileTask } from './ngc-compile.task';
 import { CopyTsconfigNgcTask } from './copy-tsconfig-ngc.task';
-import { CopyPackageMarkdownFilesTask} from './copy-package-markdown-files.task';
+import { CopyFilesToOutdirTask } from './copy-files-to-outdir.task';
 
 /**
  * Main taks that manages all dependencies between other tasks
@@ -13,14 +13,14 @@ export class MainTask {
   private inlineTask: InlineHtmlCssTask;
   private copyTsconfigTask: CopyTsconfigNgcTask;
   private ngcTask: NgcCompileTask;
-  private copyPackageMd: CopyPackageMarkdownFilesTask;
+  private copyFilesOutdirTask: CopyFilesToOutdirTask;
 
   constructor() {
     this.deleteTask = new DeleteDestFolderTask();
     this.inlineTask = new InlineHtmlCssTask();
     this.copyTsconfigTask = new CopyTsconfigNgcTask();
     this.ngcTask = new NgcCompileTask();
-    this.copyPackageMd = new CopyPackageMarkdownFilesTask();
+    this.copyFilesOutdirTask = new CopyFilesToOutdirTask();
   }
 
   registerAllTasks(argv: any, onError: Function) {
@@ -28,11 +28,11 @@ export class MainTask {
     const inlineTaskName = this.inlineTask.registerTask(argv, onError, [deleteTaskName]);
     const copyTsconfigTaskName = this.copyTsconfigTask.registerTask(argv, onError, [inlineTaskName]);
     const ngcTaskName = this.ngcTask.registerTask(argv, onError, [copyTsconfigTaskName]);
-    const copyPackageMdTaskName = this.copyPackageMd.registerTask(argv, onError, [ngcTaskName]);
+    const copyFilesOutdirTaskName = this.copyFilesOutdirTask.registerTask(argv, onError, [ngcTaskName]);
 
     const taskName = 'main-task';
 
-    gulp.task(taskName, [copyPackageMdTaskName]);
+    gulp.task(taskName, [copyFilesOutdirTaskName]);
 
     return taskName;
   }
